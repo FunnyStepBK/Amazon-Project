f67826dc03938a5a72af71f622332b7e7a896c54
FunnyStepBK <radekama372@gmail.com>
2024-03-29 17:37:58 +0530

    Made the cart quantity interactive

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -79,7 +79,14 @@ document.querySelectorAll('.js-cart-btn')
           quantity: 1
         });
       };
-      console.log(cart);
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerText = cartQuantity;
     });
   });
 ;
